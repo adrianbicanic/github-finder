b516d6afdf13b29a5af28b8d3e97e432c743568d
adrianbicanic <adibicanic@gmail.com>
2017-08-31 11:35:02 +0200

    chore(fetch): add isomorphic-fetch lib

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,7 +3,13 @@ import React from 'react';
 import Input from 'react-toolbox/lib/input/Input';
 
 const SearchBar = () => {
-  return <Input type='text' label='Search GitHub Users' name='search'/>;
+  return (
+    <Input
+      type="text"
+      label="Search GitHub Users"
+      name="search"
+    />
+  );
 }
 
 export default SearchBar;
